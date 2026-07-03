f149acca73f25a477982c34450649bb8225bec0c
Gabrielalmeida49 <bielsilvalmeida8888@gmail.com>
2026-07-03 11:29:02 -0300

    fix: suporte para links diretos de imagens externas

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -6,13 +6,25 @@ class Category extends Model {
 			{
 				name: Sequelize.STRING,
 				path: Sequelize.STRING,
+			// url: {
+			// 		type: Sequelize.VIRTUAL,
+			// 		get() {
+			// 			const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
+			// 			return `${serverUrl}/category-file/${this.path}`;
+			// 		},
+			// 	},
+
 			url: {
-					type: Sequelize.VIRTUAL,
-					get() {
-						const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
-						return `${serverUrl}/category-file/${this.path}`;
-					},
-				},
+	type: Sequelize.VIRTUAL,
+	get() {
+		// Se o path já for um link de internet (como o do GitHub), usa ele direto!
+		if (this.path && this.path.startsWith('http')) {
+			return this.path;
+		}
+		const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
+		return `${serverUrl}/category-file/${this.path}`;
+	},
+},
 			},
 			{
 				sequelize,
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -8,13 +8,25 @@ class Product extends Model {
 				price: Sequelize.INTEGER,
 				path: Sequelize.STRING,
 				offer: Sequelize.BOOLEAN,
+			// url: {
+			// 		type: Sequelize.VIRTUAL,
+			// 		get() {
+			// 			const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
+			// 			return `${serverUrl}/product-file/${this.path}`;
+			// 		},
+			// 	},
+
 			url: {
-					type: Sequelize.VIRTUAL,
-					get() {
-						const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
-						return `${serverUrl}/product-file/${this.path}`;
-					},
-				},
+	type: Sequelize.VIRTUAL,
+	get() {
+		// Se o path já for um link de internet, usa ele direto!
+		if (this.path && this.path.startsWith('http')) {
+			return this.path;
+		}
+		const serverUrl = process.env.RENDER_EXTERNAL_URL || 'http://localhost:3001';
+		return `${serverUrl}/product-file/${this.path}`;
+	},
+},
 			},
 			{
 				sequelize,
